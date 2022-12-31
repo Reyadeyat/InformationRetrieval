14cd08cd3b5caa54dd28a977c3929daa0a0fa742
reyadeyat <code@reyadeyat.net>
2022-12-31 23:05:02 +0200

    Reyadeyat NLP work 2022.12.31 - 05:45 pm

--- InformationRetrieval/src/main/java/net/reyadeyat/nlp/information/retrieval/parser/InformationRetrievalParserArabic.java
+++ InformationRetrieval/src/main/java/net/reyadeyat/nlp/information/retrieval/parser/InformationRetrievalParserArabic.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2022 Reyadeyat
+ *
+ * Reyadeyat/NLP is licensed under the
+ * BSD 3-Clause "New" or "Revised" License
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://reyadeyat.net/NLP.LICENSE  
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.reyadeyat.nlp.information.retrieval.parser;
 
 import net.reyadeyat.nlp.information.retrieval.data.structure.ParsingDocument;
@@ -10,7 +27,7 @@ import java.util.ArrayList;
  * @author Mohammad Nabil Mostafa
  * <a href="mailto:code@reyadeyat.net">code@reyadeyat.net</a>
  * 
- * @since 2022.12.31
+ * @since 2023.01.01
  */
 public class InformationRetrievalParserArabic implements InformationRetrievalParser {
     private ParsingDocument parsing_document;
